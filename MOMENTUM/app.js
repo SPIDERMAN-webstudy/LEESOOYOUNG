b6005752168e06707460fda0866a26faab2dce09
sooyoung <itsurboiace00@naver.com>
2021-12-27 16:34:00 +0900

    DAY 6

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,2 +1,17 @@
 const title=document.querySelector(".hello h1");
-title.innerText="Hello!";
+
+function handleTitleClick(){
+    title.style.color="blue";
+}
+
+function handleMouseEnter(){
+    title.innerText="Mouse is here!";
+}
+
+function handleMouseLeave(){
+    title.innerText="Mouse is gone!";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
