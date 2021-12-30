740d43e97528c39574563b9df9606b96be286bce
sooyoung <itsurboiace00@naver.com>
2021-12-30 12:31:36 +0900

    DAY 9

--- MOMENTUM/background.js
+++ MOMENTUM/background.js
@@ -1,14 +1,17 @@
 const images = [
     "bg1.jpg",
     "bg2.jpg",
-    "bg3.jpg",
-    "bg4.jpg"
+    "bg3.png",
+    "bg4.jpg",
+    "bg5.jpg",
+    "bg6.jpg",
+    "bg7.jpg",
+    "bg8.jpg",
+    "bg9.jpg",
+    "bg10.jpg",
 ];
+const BG = document.querySelector("body");
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
-
-bgImage.src = chosenImage;
-
-document.body.appendChild(bgImage);
+BG.style.backgroundImage = `url(${chosenImage})`;
