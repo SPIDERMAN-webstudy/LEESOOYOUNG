41a86fa00e4ab48f9bbdd0e4958925682c160ebd
sooyoung <itsurboiace00@naver.com>
2022-01-14 19:57:32 +0900

    DAY 16

--- react-germany/src/components/ExpenseItem.js
+++ react-germany/src/components/ExpenseItem.js
@@ -1,16 +1,13 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = "Borussia Dortmund";
-  const expenseTitle = "Erling Holland";
-  const expenseAmount = 220;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate}</div>
+      <div>{props.date}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}M</div>
+        <h2>{props.title}</h2>
+        <h3 className="expense-item__price">${props.amount}M</h3>
       </div>
     </div>
   );
