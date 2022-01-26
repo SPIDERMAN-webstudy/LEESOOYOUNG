623cc0f02d7ebfc45f81635eddb679aa246817f4
sooyoung <itsurboiace00@naver.com>
2022-01-26 19:22:32 +0900

    DAY 22

--- react-soopangeats/src/App.js
+++ react-soopangeats/src/App.js
@@ -1,10 +1,29 @@
-import "./App.css";
+import { useState } from "react";
+import Cart from "./components/Cart/Cart";
+
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [theshow, setTheshow] = useState(false);
+
+  const showCartHandler = () => {
+    setTheshow(true);
+  };
+
+  const hideCartHandler = () => {
+    setTheshow(false);
+  };
+
   return (
-    <div>
-      <h1>Let's get started!</h1>
-    </div>
+    <CartProvider>
+      {theshow && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
 }
 
