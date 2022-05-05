83658f88d361129b418df7d7933e648ad726f958
sooyoung <itsurboiace00@naver.com>
2022-05-06 01:46:22 +0900

    [05.05] next.js

--- nextjs_intro/pages/about.js
+++ nextjs_intro/pages/about.js
@@ -1,8 +1,11 @@
+import Head from "next/head";
 import NavBar from "../components/NavBar";
+import Seo from "../components/Seo";
 
 export default function about() {
   return (
     <div>
+      <Seo title="about" />
       <h1>about us</h1>
     </div>
   );
