87e35ffbe98e58df5d9304f402fdad7d9f91345c
sooyoung <itsurboiace00@naver.com>
2022-05-26 01:38:14 +0900

    유저 로그인 (auth.signInWithEmailAndPassword(id,password))

--- firebase-auth-lesson-4/firebase-auth-lesson-4/scripts/auth.js
+++ firebase-auth-lesson-4/firebase-auth-lesson-4/scripts/auth.js
@@ -3,13 +3,13 @@ const signupForm = document.querySelector("#signup-form");
 signupForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  // 정보입력
+  // 유저 정보
   const email = signupForm["signup-email"].value;
   const password = signupForm["signup-password"].value;
 
   // 메일로 회원가입
   auth.createUserWithEmailAndPassword(email, password).then((cred) => {
-    // 회원가입 모달 닫기 & form 초기화
+    // 회원가입 모달 닫기 & form 리셋
     const modal = document.querySelector("#modal-signup");
     M.Modal.getInstance(modal).close();
     signupForm.reset();
@@ -22,5 +22,24 @@ logout.addEventListener("click", (e) => {
   e.preventDefault();
   auth.signOut().then(() => {
     console.log("user signed out");
+  });
+});
+
+// 로그인
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // 유저 정보
+  const email = loginForm["login-email"].value;
+  const password = loginForm["login-password"].value;
+
+  // 유저 로그인
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log(cred.user);
+    // 로그인 모달 닫기 & form 리셋
+    const modal = document.querySelector("#modal-login");
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
   });
 });
