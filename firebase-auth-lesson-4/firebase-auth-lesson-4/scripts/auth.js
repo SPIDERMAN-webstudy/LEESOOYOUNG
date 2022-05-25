0a4fccd1a1070dba1d5f53b750a8cea9b5d577a9
sooyoung <itsurboiace00@naver.com>
2022-05-26 01:30:14 +0900

    로그아웃 (auth.signOut())

--- firebase-auth-lesson-4/firebase-auth-lesson-4/scripts/auth.js
+++ firebase-auth-lesson-4/firebase-auth-lesson-4/scripts/auth.js
@@ -9,10 +9,18 @@ signupForm.addEventListener("submit", (e) => {
 
   // 메일로 회원가입
   auth.createUserWithEmailAndPassword(email, password).then((cred) => {
-    console.log(cred.user);
     // 회원가입 모달 닫기 & form 초기화
     const modal = document.querySelector("#modal-signup");
     M.Modal.getInstance(modal).close();
     signupForm.reset();
+  });
+});
+
+// 로그아웃
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log("user signed out");
   });
 });
